adb02dd5d22011c45ed492576a6ddafbd02a2e7f
Vilhelm <52938064+h170919@users.noreply.github.com>
2025-02-02 15:33:05 +0100

    displaystub write

--- src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
+++ src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
@@ -1,24 +1,22 @@
 package no.hvl.dat110.system.controller;
 
 import no.hvl.dat110.TODO;
+import no.hvl.dat110.messaging.Message;
 import no.hvl.dat110.rpc.*;
 
 public class DisplayStub extends RPCLocalStub {
 
-	public DisplayStub(RPCClient rpcclient) {
-		super(rpcclient);
-	}
-	
-	public void write (String message) {
-		
-		// TODO - START
-		
-		// implement marshalling, call and unmarshalling for write RPC method
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-	}
+    public DisplayStub(RPCClient rpcclient) {
+        super(rpcclient);
+    }
+
+    public void write(String message) {
+
+        // TODO - START
+        byte[] request = RPCUtils.marshallString(message);
+        byte[] reply = rpcclient.call((byte) Common.WRITE_RPCID, request);
+        RPCUtils.unmarshallVoid(reply);
+        // TODO - END
+
+    }
 }
